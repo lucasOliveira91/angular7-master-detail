ae3b1693fd974420743f0db6ff48f1f1442d004a
Lucas <ldoliveira91@outlook.com>
2019-04-26 07:34:01 -0300

    adds category service

---
+++ src/app/pages/categories/shared/category.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Category } from './category.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  private apiPath: string = 'api/categories'
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAll(): Observable<Category[]> {
+    return this.http.get<Category[]>(`${this.apiPath}`)
+  }
+
+  getById(id: number): Observable<Category> {
+    return this.http.get<Category>(`${this.apiPath}/${id}`);
+  }
+
+  create(category: Category): Observable<Category> {
+    return this.http.post(`${this.apiPath}`,  category);
+  }
+
+  update(category: Category) {
+    this.http.put(`${this.apiPath}/${category.id}`, category);
+  }
+
+  delete(id: number) {
+    this.http.delete(`${this.apiPath}/${id}`);
+  }
+}
