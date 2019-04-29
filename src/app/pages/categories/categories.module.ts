02523a2c5f568b95a05755c560f93742a7530246
Lucas Daniel Borges de Oliveira <lukys_taylor@hotmail.com>
2019-04-29 19:17:12 -0300

    improving category form template

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -5,10 +5,12 @@ import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
 import { CategoryService } from './shared/category.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CategoryListComponent, CategoryFormComponent],
   imports: [
+    ReactiveFormsModule,
     CommonModule,
     CategoriesRoutingModule
   ],
--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -1,15 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CategoryService } from '../shared/category.service';
+import { toastr } from 'toastr';
+import { Category } from '../shared/category.model';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
   styleUrls: ['./category-form.component.css']
 })
-export class CategoryFormComponent implements OnInit {
+export class CategoryFormComponent implements OnInit, AfterContentChecked {
 
-  constructor() { }
+  currentAction: string = 'new';
+  categoryForm: FormGroup;
+  pageTitle: string = 'Cadastro de nova categoria';
+  serverErrorMessages: string[] = null;
+  submittingForm: boolean = false;
+  category: Category;
+
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private categoryService: CategoryService
+  ) { }
 
   ngOnInit() {
+    this.setCurrentAction();
+    this.buildCategoryForm();
+    this.loadCategory();
   }
 
+  ngAfterContentChecked() {
+    this.setPageTitle();
+  }
+
+  private setCurrentAction() {
+    if (this.route.snapshot.url[0].path !== 'new') {
+      this.currentAction = 'edit'
+    }
+  }
+
+  private buildCategoryForm() {
+    this.categoryForm = this.fb.group({
+      id: [null],
+      name: [null, [Validators.required, Validators.minLength(2)]],
+      description: [null]
+    });
+  }
+
+  private loadCategory() {
+    if (this.currentAction === 'edit') {
+      this.route.paramMap.pipe(switchMap(params => this.categoryService.getById(+params.get('id')))).subscribe(category => {
+        this.category = category;
+        this.categoryForm.patchValue(this.category);
+      }, error => alert('Ocorreu um erro'));
+    }
+  }
+
+  private setPageTitle() {
+    if(this.currentAction === `edit` && this.category){
+      this.pageTitle = `Edição de Categoria ${this.category.name}`;
+    }
+  }
 }
